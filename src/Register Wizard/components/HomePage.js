f4c2e42371fdf17dfb465ee586e3b0049252cea2
IsraelaZimru <slzimro@gmail.com>
2021-06-01 15:53:16 +0300

    edite page1 and router

---
+++ src/Register Wizard/components/HomePage.js
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link } from 'react-router-dom'
+
+export default function HomePage() {
+    return <div className="text-center my-lg-5">
+        <h1 className="display-3">Click below <br></br> to Start registration</h1>
+        <hr style={{ width: "700px", border: "3px solid black" }}></hr>
+        <Link to={'/phase_1'}> <Button style={{ width: "200px", heigth: "300px" }} className="btn btn-primary"> start </Button></Link>
+    </div>
+}
